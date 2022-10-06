f3eca30df779d9a7480e6f2074e8de821e0b3324
fzahidah <zahidah2000@gmail.com>
2022-10-06 11:52:27 +0800

    Add middleware (e22)

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -8,27 +8,38 @@ use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
-    public function index() {
+    public function __construct()
+    {
+        // ->except('[]') to not include specified method under middleware
+        // ->only('[]') to include only specified method under middleware
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
         $customers = Customer::all();
 
         return view('customers.index', compact('customers'));
     }
 
-    public function create() {
+    public function create()
+    {
         $companies = Company::all();
         $customer = new Customer();
 
         return view('customers.create', compact('companies', 'customer'));
     }
 
-    public function store() {
+    public function store()
+    {
         // dd(request('name', 'no name'));
         Customer::create($this->validateRequest());
 
         return redirect('/customers');
     }
 
-    public function show(Customer $customer) {
+    public function show(Customer $customer)
+    {
         // use firstOrFail() instead of find() so that if the record
         // does not exist, it will return 404 page instead of crashing
         // $customer = Customer::where('id', $cid)->firstOrFail();
@@ -39,23 +50,27 @@ class CustomerController extends Controller
         return view('customers.show', compact('customer'));
     }
 
-    public function edit(Customer $customer) {
+    public function edit(Customer $customer)
+    {
         $companies = Company::all();
         return view('customers.edit', compact('customer', 'companies'));
     }
 
-    public function update(Customer $customer) {
+    public function update(Customer $customer)
+    {
         $customer->update($this->validateRequest());
 
         return redirect(url('/customers\/') . $customer->id);
     }
 
-    public function destroy(Customer $customer) {
+    public function destroy(Customer $customer)
+    {
         $customer->delete();
         return redirect('/customers');
     }
 
-    private function validateRequest() {
+    private function validateRequest()
+    {
         return request()->validate([
             'name' => 'bail|required|min:3',
             // 'optionalvalue' => 'nullable|min:3'
