378c557ec6584f5164a0847027b021d2c7069803
fzahidah <zahidah2000@gmail.com>
2022-10-10 09:21:24 +0800

    Add new command (e34)

---
+++ app/Console/Commands/AddCompanyCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Company;
+use Illuminate\Console\Command;
+
+class AddCompanyCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'contact:company {name} {phone=N/A}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Adds a new company.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $company = Company::create([
+            'name' => $this->argument('name'),
+            'phone' => $this->argument('phone')
+        ]);
+
+        $this->info('Added new company: ' . $company->name);
+        // $this->warn('I am a warning');
+        // $this->error('I am an error');
+    }
+}
