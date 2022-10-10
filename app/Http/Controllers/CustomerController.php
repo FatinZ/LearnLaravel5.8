4e1a5faeda5b52d77a3d3e1c9427c58d279800a1
fzahidah <zahidah2000@gmail.com>
2022-10-10 15:54:19 +0800

    Change lazy loading to eager loading (e42)

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -21,7 +21,7 @@ class CustomerController extends Controller
 
     public function index()
     {
-        $customers = Customer::all();
+        $customers = Customer::with('company')->get();
 
         return view('customers.index', compact('customers'));
     }
